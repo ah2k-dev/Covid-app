89b4bb9b8d5575695061842af785340ce5b0f606
ahmedhassan2k <ah2k.dev@gmail.com>
2022-08-20 23:39:33 +0500

    countries component

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Countries = () => {
-  return (
-    <div>Countries</div>
-  )
-}
+  const [countries, setCountries] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      setCountries(data.Countries);
+      console.log(data.Countries);
+    };
+    getData();
+  }, []);
+  if (!countries) {
+    return <div>Loading...</div>;
+  }
+  return <div className="GlobalContainer">
+    {
+        countries.map((value, index)=>{
+            delete value.Premium;
+            delete value.ID
+            return <CountriesBox value={value} key={index} />
+        })
+    }
+  </div>;
+};
 
-export default Countries
+export default Countries;
+
+const CountriesBox = (props) => {
+    return (
+    <div className="CountriesBox">
+        <h3>{props.value.Country}</h3>
+        {
+            Object.keys(props.value).map((val, ind)=>{
+                return <div className="CountryValues">
+                    <span>{val} :</span>
+                    <span>{props.value[val]}</span>
+                </div>
+            })
+        }
+    </div>
+    )
+}
--- src/components/Global.js
+++ src/components/Global.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Countries from "./Countries";
 
 const Global = () => {
   const [global, setGlobal] = useState();
